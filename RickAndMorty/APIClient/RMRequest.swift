8d873370b89722309e9917fe9cc064df284a543f
bahittinn <bahittin84@gmail.com>
2023-07-16 00:09:51 +0300

    Request Model

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -10,6 +10,54 @@ import Foundation
 
 /// Object that represents a single API call
 final class RMRequest {
+    //Base url
+    //Endpoint
+
+    private struct Constants {
+        static let baseURL = "https://rickandmortyapi.com/api"
+    }
+    private let endPoint: RMEndpoint
     
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+
+    private var urlString: String {
+        var string = Constants.baseURL
+        string += "/"
+        string += endPoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            string += "/"
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    //MARK: - Public
+    init(endPoint: RMEndpoint,
+         pathComponents: [String] = [],
+         queryParameters: [URLQueryItem] = []
+    ) {
+        self.endPoint = endPoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
 
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -14,8 +14,6 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
-        print(RMEndpoint(rawValue: "character") ?? "")
-        // Do any additional setup after loading the view.
     }
     
  
