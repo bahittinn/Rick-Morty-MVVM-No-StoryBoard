8a04f0d7a19f618ef7d34d370eeb03ac99928578
bahittinn <bahittin84@gmail.com>
2023-07-16 20:52:22 +0300

    deneme

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 /// Controller to show and search for Characters
 final class RMCharacterViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
     }
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "deneme"
+        return label
+    }()
 }
